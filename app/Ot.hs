d6910269adff7ec7ca43748f85e6c9fe1d565600
habutaso <habutaso@gmail.com>
2020-11-08 20:43:50 +0900

    add ot test

--- app/Ot.hs
+++ app/Ot.hs
@@ -3,27 +3,32 @@ module Ot where
 import Data.Char
 import Data.Maybe
 
-data Tree t = Node t [Tree t]
+data Tr = 
+      Nill
+    | Str String
+    | Nod Tr Tr
+    | Child [Tr]
     deriving (Eq, Show)
 
--- class OTBase x cmd where
---     interp :: cmd -> x -> Maybe x
---     it :: cmd -> cmd -> Bool -> [cmd]
+data Tree t = 
+      Node t [Tree t] 
+    deriving (Eq, Show)
 
-data OTBase x cmd =
-    BuildOTBase (cmd -> x -> Maybe x) (cmd -> cmd -> Bool -> [cmd])
+data LabelCommand = 
+      UStr String
+    | UInt Int
+    deriving (Show)
 
-data LabelCommand =
-    | Update
+data ListCommand =
+      TreeInsert Int [Tree String]
+    | TreeRemove Int [Tree String]
+    | EditLabel LabelCommand
+    deriving (Show)
 
-data ListCommand cmd a =
-      TreeInsert Int [Tree a]
-    | TreeRemove Int [Tree a]
-    | EditLabel cmd
-
-data TreeCommand cmd a =
-      Atomic (ListCommand cmd a)
-    | OpenRoot Int (TreeCommand cmd a)
+data TreeCommand =
+      Atomic ListCommand
+    | OpenRoot Int TreeCommand
+    deriving (Show)
 
 tr_ins :: Int -> Int -> Int -> Int
 tr_ins len n1 n2 =
@@ -35,14 +40,6 @@ tr_rem len n1 n2
     | (n2 + len) <= n1 = Just (n1 - len)
     | otherwise        = Nothing
     
-interp :: (OTBase a1 a2) -> a2 -> a1 -> Maybe a1
-interp (BuildOTBase in1 _) = in1
-
-it :: (OTBase a1 a2) -> a2 -> a2 -> Bool -> [a2]
-it (BuildOTBase _ it1) = it1
-
-
-weak_cons :: a -> Maybe [a] -> Maybe [a]
 weak_cons x = (>>= (\xs -> Just (x : xs)))
 
 cat :: [a] -> [a] -> [a]
@@ -83,41 +80,44 @@ nodeW :: a -> Maybe [Tree a] -> Maybe (Tree a)
 nodeW t (Just ls) = Just (Node t ls)
 nodeW t Nothing   = Nothing
 
-list_interp :: (Eq t) => OTBase t cmd -> ListCommand cmd t -> Tree t -> Maybe (Tree t)
-list_interp ot (TreeInsert n l) (Node x0 ls) = nodeW x0 (ins n l ls)
-list_interp ot (TreeRemove n l) (Node x0 ls) = nodeW x0 (rm n l ls)
-list_interp ot (EditLabel c) (Node x0 ls) =
-    let interpreted = interp ot c x0 in
-    if isJust interpreted 
-    then let Just x' = interpreted in Just (Node x' ls)
-    else Nothing
+text_interp :: LabelCommand -> Tree String -> Maybe (Tree String)
+text_interp (UStr s) (Node x0 ls) = Just (Node s ls)
+text_interp (UInt i) (Node x0 ls) = Just (Node (show i) ls)
+text_interp _ _ = Nothing
+
+list_interp :: ListCommand -> Tree String -> Maybe (Tree String)
+list_interp (TreeInsert n l) (Node x0 ls) = nodeW x0 (ins n l ls)
+list_interp (TreeRemove n l) (Node x0 ls) = nodeW x0 (rm n l ls)
+list_interp (EditLabel c) node = text_interp c node
+
+tree_interp :: TreeCommand -> Tree String -> Maybe (Tree String)
+tree_interp (Atomic c) (Node x0 ls) = list_interp c (Node x0 ls)
+tree_interp (OpenRoot n c) (Node x0 ls) = 
+    nodeW x0 (rplc n ((nth n ls) >>= (tree_interp c)) ls) 
 
 
-tree_interp :: (Eq t) => OTBase t cmd -> TreeCommand cmd t -> Tree t -> Maybe (Tree t) 
-tree_interp ot (Atomic c) (Node x0 ls) = list_interp ot c (Node x0 ls)
-tree_interp ot (OpenRoot n c) (Node x0 ls) = 
-    nodeW x0 (rplc n ((nth n ls) >>= (tree_interp ot c)) ls) 
+text_it :: LabelCommand -> LabelCommand -> Bool -> [LabelCommand]
+text_it c1 c2 f = if f then [c2] else [c2]
 
-list_it :: (OTBase a b) -> (ListCommand b c)
-           -> (ListCommand b c) -> Bool -> [ListCommand b c]
 
-list_it ot (TreeInsert n1 l1) (TreeInsert n2 l2) flag
+list_it :: ListCommand -> ListCommand -> Bool -> [ListCommand]
+list_it (TreeInsert n1 l1) (TreeInsert n2 l2) flag
     | n1 == n2 &&      flag  = op1 : []
     | n1 == n2 && (not flag) = (TreeInsert (n1 + len2) l1) : []
     | otherwise              = (TreeInsert (tr_ins len2 n1 n2) l1) : []
     where op1 = TreeInsert n1 l1
           len2 = length l2
 
-list_it ot (TreeInsert n1 l1) (TreeRemove n2 l2) _
+list_it (TreeInsert n1 l1) (TreeRemove n2 l2) _
     | n1 < n2          = op1 : []
     | (n2 + len2) < n1 = (TreeInsert (n1 - len2) l1) : []
     | otherwise        = []
     where op1 = TreeInsert n1 l1
           len2 = length l2
 
-list_it ot (TreeInsert n1 l1) (EditLabel _) _ = (TreeInsert n1 l1) : []
+list_it (TreeInsert n1 l1) (EditLabel _) _ = (TreeInsert n1 l1) : []
 
-list_it ot (TreeRemove n1 l1) (TreeInsert n2 l2) flag
+list_it (TreeRemove n1 l1) (TreeInsert n2 l2) flag
     | n1 + len1 < n2 = op1 : []
     | n2 < n1        = (TreeRemove (n1 + len2) l1) : []
     | otherwise      =
@@ -128,7 +128,7 @@ list_it ot (TreeRemove n1 l1) (TreeInsert n2 l2) flag
     where op1 = TreeRemove n1 l1
           len1 = length l1; len2 = length l2
 
-list_it ot (TreeRemove n1 l1) (TreeRemove n2 l2) flag
+list_it (TreeRemove n1 l1) (TreeRemove n2 l2) flag
     | (n2 + len2) < n1 = (TreeRemove (n1 - len2) l1) : []
     | (n1 + len1) < n2 = (TreeRemove n1 l1) : []
     | n2 < n1          = (TreeRemove n2 (cut l1 0 ((len2 + n2) - n1))) : []
@@ -136,46 +136,46 @@ list_it ot (TreeRemove n1 l1) (TreeRemove n2 l2) flag
     where len1 = length l1
           len2 = length l2
 
-list_it ot (TreeRemove n1 l1) (EditLabel _) flag = (TreeRemove n1 l1) : []
+list_it (TreeRemove n1 l1) (EditLabel _) flag = (TreeRemove n1 l1) : []
 
--- Label用のコマンドをつくる
-list_it ot (EditLabel c1) (EditLabel c2) flag =
-    map (\y -> EditLabel y) (it ot c1 c2 flag)
+list_it (EditLabel c1) (EditLabel c2) flag =
+    map (\y -> EditLabel y) (text_it c1 c2 flag)
 
-list_it ot (EditLabel c1) _ _ = (EditLabel c1) : []
+list_it (EditLabel c1) _ _ = (EditLabel c1) : []
 
 
-tree_it :: (Eq t) => OTBase t cmd -> TreeCommand cmd t
-           -> TreeCommand cmd t -> Bool -> [TreeCommand cmd t]
 
-tree_it ot (Atomic c1) (Atomic c2) flag =
-    map (\a -> Atomic a) (list_it ot c1 c2 flag)
+tree_it :: TreeCommand -> TreeCommand -> Bool -> [TreeCommand]
 
-tree_it ot (Atomic (TreeRemove n1 l)) (OpenRoot n2 c2) flag = 
+tree_it (Atomic c1) (Atomic c2) flag =
+    map (\a -> Atomic a) (list_it c1 c2 flag)
+
+tree_it (Atomic (TreeRemove n1 l)) (OpenRoot n2 c2) flag = 
     let op1 = Atomic (TreeRemove n1 l) in
     let notrem = isNothing (tr_rem (length l) n2 n1) in
     let j = (n2 - n1) in
-    let binded = (nth j l) >>= (tree_interp ot c2) in
+    let binded = (nth j l) >>= (tree_interp c2) in
     let rplced = rplc j binded l in
     let doesrplc = isJust rplced in
     if notrem && doesrplc
     then let Just l' = rplced in (Atomic (TreeRemove n1 l')) : []
     else op1 : []
 
-tree_it ot (Atomic c) (OpenRoot _ _) flag = (Atomic c) : []
+tree_it (Atomic c) (OpenRoot _ _) flag = (Atomic c) : []
 
-tree_it ot (OpenRoot n1 c1) (Atomic (TreeInsert n2 l2)) flag =
+tree_it (OpenRoot n1 c1) (Atomic (TreeInsert n2 l2)) flag =
     (OpenRoot (tr_ins (length l2) n1 n2) c1) : []
 
-tree_it ot (OpenRoot n1 c1) (Atomic (TreeRemove n2 l2)) flag =
+tree_it (OpenRoot n1 c1) (Atomic (TreeRemove n2 l2)) flag =
     if isjust then let Just n1' = remmed in (OpenRoot n1' c1) : []
               else []
     where remmed = tr_rem (length l2) n1 n2
           isjust = isJust remmed
 
-tree_it ot (OpenRoot n1 c1) (Atomic (EditLabel c2)) flag = (Atomic (EditLabel c2)) : []
+tree_it (OpenRoot n1 c1) (Atomic (EditLabel c2)) flag = 
+    (Atomic (EditLabel c2)) : []
 
-tree_it ot (OpenRoot n1 c1) (OpenRoot n2 c2) flag =
+tree_it (OpenRoot n1 c1) (OpenRoot n2 c2) flag =
     if n1 == n2 
-    then map (\a -> OpenRoot n1 a) (tree_it ot c1 c2 flag)
+    then map (\a -> OpenRoot n1 a) (tree_it c1 c2 flag)
     else (OpenRoot n1 c1) : []
--- ignore/OtTest.hs
+++ ignore/OtTest.hs
@@ -1,9 +1,24 @@
-import Ot2
+import Ot
 
 import Data.Char
 
 tree = Node "r" [Node "a" [], Node "b" [], Node "c" []]
 
-op1 = Atomic (TreeInsert 2 (Node "d" []))
-op2 = Atomic (TreeRemove 0 (Node "a" []))
-test = tree_it op1 op2
+op1 = Atomic (TreeInsert 2 ((Node "d" []):[]))
+op2 = Atomic (TreeRemove 0 ((Node "a" []):[]))
+
+op3 = Atomic (TreeInsert 3 ((Node "e" [Node "f" [], Node "g" []]):[]))
+op4 = Atomic (EditLabel (UStr "hello"))
+op5 = Atomic (EditLabel (UStr "world"))
+
+trans op1 op2 flag =
+    let op' = tree_it op2 op1 flag in
+    let t' = tree_interp op1 tree in
+    (tree_interp (head op')) =<< t'
+
+test op1 op2 = do
+    putStrLn $ "\ntree: " ++ (show tree)
+    putStrLn $ "op1: " ++ (show op1) ++ "\nop2: " ++ (show op2)
+    putStrLn $ "\ntransformed:"
+    putStrLn $ "s12: " ++ (show $ trans op1 op2 False)
+    putStrLn $ "s21: " ++ (show $ trans op2 op1 True)
